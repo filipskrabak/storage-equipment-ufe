c2e90706929540e7dd729ef1fbe434e984de3e8d
filipskrabak <xskrabakf@stuba.sk>
2025-03-22 21:52:56 +0100

    init

---
+++ storage-equipment-ufe/src/components/steq-orders-list/steq-orders-list.tsx
@@ -0,0 +1,17 @@
+import { Component, Host, h } from '@stencil/core';
+
+@Component({
+  tag: 'steq-orders-list',
+  styleUrl: 'steq-orders-list.css',
+  shadow: true,
+})
+export class SteqOrdersList {
+  render() {
+    return (
+      <Host>
+        Empty
+        <slot></slot>
+      </Host>
+    );
+  }
+}
---
+++ storage-equipment-ufe/src/components/steq-orders-list/test/steq-orders-list.spec.tsx
@@ -0,0 +1,18 @@
+import { newSpecPage } from '@stencil/core/testing';
+import { SteqOrdersList } from '../steq-orders-list';
+
+describe('steq-orders-list', () => {
+  it('renders', async () => {
+    const page = await newSpecPage({
+      components: [SteqOrdersList],
+      html: `<steq-orders-list></steq-orders-list>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <steq-orders-list>
+        <mock:shadow-root>
+          <slot></slot>
+        </mock:shadow-root>
+      </steq-orders-list>
+    `);
+  });
+});
